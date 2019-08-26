c1462bd14be9000afc6d9efc17086ca9087f6c23
Sam Coco <Samuel.HenriAC@outlook.com>
2019-08-27 00:56:11 +0100

    Constants class to decrease likelihood of typo bug

---
+++ src/main/java/matrix/webapp/Constant.java
@@ -0,0 +1,9 @@
+package matrix.webapp;
+
+public final class Constant {
+    public static final String MATRIX_1_ROWS = "matrix1Rows";
+    public static final String MATRIX_1_COLUMNS = "matrix1Columns";
+    public static final String MATRIX_2_ROWS = "matrix2Rows";
+    public static final String MATRIX_2_COLUMNS = "matrix2Columns";
+
+}
