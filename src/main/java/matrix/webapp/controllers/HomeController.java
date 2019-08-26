7a1f4e3bb04a09d216910873bf8e2ce45be281de
Sam Coco <Samuel.HenriAC@outlook.com>
2019-08-26 22:52:04 +0100

    Client data mapped to 'data' parameter

--- src/main/java/matrix/webapp/controllers/HomeController.java
+++ src/main/java/matrix/webapp/controllers/HomeController.java
@@ -1,22 +1,26 @@
 package matrix.webapp.controllers;
 
-import matrix.webapp.models.MatrixSizeModel;
+import matrix.webapp.models.MatrixModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 @Controller
 public class HomeController {
 
     @RequestMapping("")
-    public String index(){
+    public String home(){
         return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String executeCalculation(@RequestBody MatrixSizeModel size){
-        // calculation controller
+    public String executeCalculation(@RequestBody Map<String, String> data){
+        System.out.println("\n\n" + "RECEIVED DATA:" + data.toString()); // todo-REMOVE
+        ArrayList<Double[][]> matrices =  MatrixModel.extractMatrices(data);
         return "index";
     }
 }
