c2caaa6eceb389928f121cee96dd9c4d6cc96523
Sam Coco <Samuel.HenriAC@outlook.com>
2019-08-27 17:01:32 +0100

    JSON response added

--- src/main/java/matrix/webapp/controllers/HomeController.java
+++ src/main/java/matrix/webapp/controllers/HomeController.java
@@ -2,13 +2,13 @@ package matrix.webapp.controllers;
 
 import matrix.webapp.Constant;
 import matrix.webapp.models.MatrixModel;
-import matrix.webapp.services.MatrixService;
 import matrix.webapp.models.MatrixSizeModel;
-
+import matrix.webapp.services.MatrixService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,7 +26,8 @@ public class HomeController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String executeCalculation(@RequestBody Map<String, String> data){
+    @ResponseBody
+    public Map<String, String> executeCalculation(@RequestBody Map<String, String> data){
 
         System.out.println("\n\n" + "RECEIVED DATA:" + data.toString()); // todo-REMOVE
 
@@ -35,11 +36,11 @@ public class HomeController {
         String operator = data.get("operator");
 
         if(operator.equals("+") || operator.equals("-") || operator.equals("x")){
-            matrix1 = new MatrixService("Matrix 1", size.get(Constant.MATRIX_1_ROWS),
+            matrix1 = new MatrixService("MATRIX 1", size.get(Constant.MATRIX_1_ROWS),
                    size.get(Constant.MATRIX_1_COLUMNS));
             matrix1.setMatrix(MatrixService.doubleArrayUnboxer(matrices.get(0)));
 
-            matrix2 = new MatrixService("Matrix 2", size.get(Constant.MATRIX_2_ROWS),
+            matrix2 = new MatrixService("MATRIX 2", size.get(Constant.MATRIX_2_ROWS),
                     size.get(Constant.MATRIX_2_COLUMNS));
             matrix2.setMatrix(MatrixService.doubleArrayUnboxer(matrices.get(1)));
         }
@@ -51,8 +52,6 @@ public class HomeController {
         } else if(operator.equals("x")){
             matrix1.multiply(matrix2);
         }
-        matrix1.printMatrix(); //todo - REMOVE
-        matrix2.printMatrix(); // todo - REMOVE
-        return "index";
+        return MatrixModel.convertArrayToMap(matrix1);
     }
 }
